bbf88ff9e27a6b24804394e4daf5bc13c6817732
Liam Cottrell <lemcottrell@gmail.com>
2023-12-15 21:44:03 +0000

    add weather icon

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
+// Event listener to get city name
 $('#search-button').on('click', function(event){
     event.preventDefault();
     let searchCity = $('#search-input').val();
-    
-    // let startDate = $('#start-year').val();
-    // console.log(startDate)
-    // let endDate = $('#end-year').val();
-    // console.log(endDate);
-    // &begin_date=20040101
-    // &end_date=20060101
 
-    
+    // Build API query
     let queryURL= "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=7ddd7e57555c4c12e3640f758afd6ed6";
    
-
+    // Fetch API data
     fetch(queryURL)
         .then(function(response){
         return response.json();
     }).then(function(data){
         console.log(data);
-        let cityName = data.city.name;
 
+        // Section variables
+        let todaySection = $('#today');
         let forecastSection = $('#forecast');
 
+        // Data category variables
+        let cityName = data.city.name;
+        let todayHeader = $('<h2>' + cityName + '</h2>');
+        let weatherIconNum = data.list[0].weather[0].icon;
+        let weatherIcon = $('<img>');
+
+        // Append to today section
+        todaySection.append(todayHeader);
+
+        // Append to forecast cards
         let card = $('<div>');
         card.append('<h3>' + cityName + '</h3>');
         card.attr("class", "card col-sm-2");
-
+        card.append(weatherIcon);
+        weatherIcon.attr("src", "https://openweathermap.org/img/wn/" + weatherIconNum + ".png");
+        weatherIcon.attr("class", "icon");
         forecastSection.append(card);
 
 
